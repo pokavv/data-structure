6b249a31610b34aad439a0389a8e6932f9e9ee70
pokavv <okaps@hanmail.net>
2022-09-20 05:35:01 +0900

    add 220920

--- ds_exam/exam9_2178.py
+++ ds_exam/exam9_2178.py
@@ -1,10 +1,32 @@
 # boj 2178
+from collections import deque
 import sys
 
+dy = (0, 1, 0, -1)
+dx = (1, 0, -1, 0)
 n, m = map(int, sys.stdin.readline().split())
 board = [input() for i in range(n)]
 
-def bfs():
-    
+def is_valid_coord(y, x):
+    return 0 <= y < n and 0 <= x < m
 
-bfs()
+def bfs():
+    checking = [[False] * m for j in range(n)]
+    checking[0][0] = True
+    dq = deque(())
+    dq.append((0, 0, 1))
+    
+    while dq:
+        y, x, d = dq.popleft()
+        nd = d + 1
+        if y == n - 1 and x == m - 1:
+            return d
+        
+        for i in range(4):
+            ny = y + dy[i]
+            nx = x + dx[i]
+            if is_valid_coord(ny, nx) and board[ny][nx] == '1' and not checking[ny][nx]:
+                checking[ny][nx] = True
+                dq.append((ny, nx, nd))
+
+print(bfs())
